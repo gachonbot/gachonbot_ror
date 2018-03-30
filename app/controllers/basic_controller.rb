adde442c19d9970121c246d04bb8fd685768bd07
EC2 Default User <ec2-user@ip-172-31-36-153.us-east-2.compute.internal>
2018-03-30 16:51:09 +0900

    day

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -12,6 +12,22 @@ class BasicController < ApplicationController
         render json: @msg, status: :ok
     end
     
+    def exchange_day(@day)
+      case @day # a_variable is the variable we want to compare
+      when "Monday"    #compare to 1
+        @day = "월요일"
+      when "Tuesday"    #compare to 2
+        @day = "화요일"
+      when "Wednesday"
+        @day = "수요일"
+      when "Thursday"
+        @day= "목요일"
+      when "Friday"
+        @day = "금요일"
+      end
+      
+    end
+    
     def chat_control
         @response = params[:content]
         @user_key = params[:user_key]
@@ -19,7 +35,7 @@ class BasicController < ApplicationController
         if @response == "시작하기"
             @msg = {
               message: {
-                  text: "안녕하세요 가천대학교 봇입니다! 궁금하신게 있으시면 다 대답해 드립니다! 대표적인 명령어는 명령어 보여줘! 로 확인 가능합니다!"
+                  text: "안녕하세요 가천대학교 봇입니다! 궁금하신게 있으시면 다 대답해 드립니다! 대표적인 명령어는 명령어 보여줘! 로 확인 가능합니다! 즐거운 #{exchange_day(Date.today.strftime("%A"))} 되세요!"
               },
               keyboard: {
                 type: "text",
@@ -27,6 +43,46 @@ class BasicController < ApplicationController
             }
             render json: @msg, status: :ok
         elsif @response.include? "학식"
+          @msg = {
+              message: {
+                  text: "오늘의 학식을 알려드릴게요! 건물을 선택해 주세요!"
+              },
+              keyboard: {
+                type: "buttons",
+                buttons: ["창조관 학식", "아름관 학식", "비전타워 학식"]
+              }
+            }
+            render json: @msg, status: :ok
+            elsif @response == "창조관 학식"
+            @msg = {
+              message: {
+                  text: "#{Time.now}"
+              },
+              keyboard: {
+                type: "text",
+              }
+            }
+            render json: @msg, status: :ok
+            elsif @response == "아름관 학식"
+            @msg = {
+              message: {
+                  text: "#{Time.now}"
+              },
+              keyboard: {
+                type: "text",
+              }
+            }
+            render json: @msg, status: :ok
+            elsif @response == "비전타워 학식"
+            @msg = {
+              message: {
+                  text: "#{Time.now}"
+              },
+              keyboard: {
+                type: "text",
+              }
+            }
+            render json: @msg, status: :ok
           url = 'http://m.gachon.ac.kr/menu/menu.jsp?gubun=A'
           page = RestClient.get(url)
            doc = Nokogiri::HTML(page)
