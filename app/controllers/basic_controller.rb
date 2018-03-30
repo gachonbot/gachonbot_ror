098fb737c000a32023c46983171a51eac7956325
EC2 Default User <ec2-user@ip-172-31-36-153.us-east-2.compute.internal>
2018-03-30 03:31:33 +0000

    ver6

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -3,7 +3,7 @@ class BasicController < ApplicationController
         @msg = {
                 message: {
                   text: "안녕하세요 가천대학교 챗봇입니다!"
-                },
+                }
                 type: "text"
               }
             render json: @msg, status: :ok
