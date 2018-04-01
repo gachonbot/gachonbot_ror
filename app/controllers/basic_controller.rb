999720d9aa14257d1b7a4185b80c2c0b4397214b
EC2 Default User <ec2-user@ip-172-31-36-153.us-east-2.compute.internal>
2018-04-01 23:17:07 +0900

    sim

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -116,7 +116,7 @@ class BasicController < ApplicationController
             }
             render json: @msg, status: :ok
         else
-          resp = HTTParty.get("http://sandbox.api.simsimi.com/request.p?key=e7501386-fca8-4723-b278-36755e917526&lc=ko&ft=1.0&text=#{CGI.unescape(@response.encode)}")
+          resp = HTTParty.get("http://sandbox.api.simsimi.com/request.p?key=e7501386-fca8-4723-b278-36755e917526&lc=ko&ft=1.0&text=#{CGI.escape(@response)}")
             @msg = {
               message: {
                   text: resp.body
