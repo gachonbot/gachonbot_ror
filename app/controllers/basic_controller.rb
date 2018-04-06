aa1d620c4480bc32c51d2582544f6da4ec2fd844
EC2 Default User <ec2-user@ip-172-31-36-153.us-east-2.compute.internal>
2018-04-06 12:51:12 +0900

    air

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -189,10 +189,33 @@ class BasicController < ApplicationController
                 @tc =  x["temperature"]["tc"].to_i.round
                 @tmin =  x["temperature"]["tmin"].to_i.round
                 @tmax =  x["temperature"]["tmax"].to_i.round
-              end            
+              end 
+              airresp = HTTParty.get("http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=Yg%2Fv2SNnJRBBLW7RzCyiLenB1dTtvBca4kgw6L9wyVzY1M224M0RlRnxwasd9FMOMrWMqgD%2Ft0b%2BMDFdg5jWig%3D%3D&numOfRows=1&pageSize=1&pageNo=1&startPage=1&stationName=#{URI.escape("수내동")}&dataTerm=DAILY&ver=1.3")
+              air = airresp.parsed_response['response']['body']['items']['item']['pm10Value'].to_i
+              if(air>150)
+                airValue = "매우나쁨"
+                elsif(air>80)
+                airValue = "나쁨"
+                elsif(air>30)
+                airValue = "보통"
+              else
+                airValue = "좋음"
+              end
               @msg = {
               message: {
-                  text: "현재 가천대의 날씨입니다!\n기상 : #{@name}\n현재온도 : #{@tc}도\n최저기온 : #{@tmin}도\n최고기온 : #{@tmax}도"
+                  text: "현재 가천대의 날씨입니다!\n기상 : #{@name}\n현재온도 : #{@tc}도\n최저기온 : #{@tmin}도\n최고기온 : #{@tmax}도\n미세먼지 농도 : #{air}\t#{airValue}"
+              },
+              keyboard: {
+                type: "text",
+              }
+            }
+            render json: @msg, status: :ok
+            elsif @response.include? "미세먼지"
+              resp = HTTParty.get("http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=Yg%2Fv2SNnJRBBLW7RzCyiLenB1dTtvBca4kgw6L9wyVzY1M224M0RlRnxwasd9FMOMrWMqgD%2Ft0b%2BMDFdg5jWig%3D%3D&numOfRows=1&pageSize=1&pageNo=1&startPage=1&stationName=#{URI.escape("수내동")}&dataTerm=DAILY&ver=1.3")
+              air = resp.parsed_response['response']['body']['items']['item']['pm10Value'].to_i
+              @msg = {
+              message: {
+                  text: "현재 가천대의 미세먼지농도 입니다!\n농도 : #{air}"
               },
               keyboard: {
                 type: "text",
@@ -242,22 +265,17 @@ class BasicController < ApplicationController
             elsif @response.include? "편의점"
             @msg = {
               message: {
-                  text: "가천관 2층(세븐일레븐), 공과대학2 4층, 프리덤광장(세븐일레븐), 비전타워A동(세븐일레븐), 비전타워B동(세븐일레븐), 예술대학1 1층, 중앙도서관 지하1층에 ٩(ᐛ)و "
+                  text: "가천관 2층(세븐일레븐), 공과대학2 4층, 프리덤광장(세븐일레븐), 비전타워A동(세븐일레븐), 비전타워B동(세븐일레븐), 예술대학1 1층, 중앙도서관 지하1층에 있습니다! ٩(ᐛ)و "
               },
               keyboard: {
                 type: "text",
               }
             }
             render json: @msg, status: :ok
-            elsif @response.include? "날씨"
-            resp = HTTParty.get("https://api2.sktelecom.com/weather/current/hourly?version=1&lat=37.450745&lon=127.128804&appkey=af357d39-420c-49b0-ac22-d29381aa2a9b")
-             resp.parsed_response["realtimeArrivalList"].each do |x|
-                  @route << x["trainLineNm"]
-                  @routeMSG << x["arvlMsg2"]
-                end
+            elsif @response.include? "카페"&&"교내"
             @msg = {
               message: {
-                  text: "현지야사랑해"
+                  text: "IT대학앞 세븐일레븐옆에 파스쿠치, 가천관2층에 Grazie, 프리덤광장에 투썸플레이스, 카페로가 있습니다! ٩(ᐛ)و "
               },
               keyboard: {
                 type: "text",
