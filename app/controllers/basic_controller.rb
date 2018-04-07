2c286282990c63777bbaf08bcd528470b6c8d040
EC2 Default User <ec2-user@ip-172-31-36-153.us-east-2.compute.internal>
2018-04-08 01:12:52 +0900

    notice

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -194,9 +194,9 @@ class BasicController < ApplicationController
               message: {
                   text: "#{info[0]}\n\n#{info[1]}\n\n#{info[2]}\n\n#{info[3]}\n\n#{info[4]}\n\n"
               },
-              "message_button": {
-               "label": "공지사항 바로가기.",
-                "url": "http://m.gachon.ac.kr/gachon/notice.jsp?boardType_seq=358"
+              message_button: {
+               label: "공지사항 바로가기.",
+                url: "http://m.gachon.ac.kr/gachon/notice.jsp?boardType_seq=358"
               },
               keyboard: {
                 type: "text",
