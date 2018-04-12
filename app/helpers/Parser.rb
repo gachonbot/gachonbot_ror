6116c03fa1b467baa4a160fbf5d2d3956da5ac62
EC2 Default User <ec2-user@ip-172-31-36-153.us-east-2.compute.internal>
2018-04-13 01:03:00 +0900

    class

--- app/helpers/Parser.rb
+++ app/helpers/Parser.rb
@@ -1,10 +1,14 @@
+require 'nokogiri'
+require 'rest-client'
+
+
 class Parser
     def food_parser
          url ="http://m.gachon.ac.kr/menu/menu.jsp"
             page = RestClient.get(url)
            doc = Nokogiri::HTML(page)
            info = doc.xpath("//*[@id=\"toggle-view\"]/li[1]/dl/dd[1]")
-           puts info.text.gsub("\r", "\r\n")
+           return info.text.gsub("\r", "\r\n")
     end
     
 end
