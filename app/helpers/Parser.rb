ed0a353eae2df6e8842d6aa186c31620dd288f26
EC2 Default User <ec2-user@ip-172-31-36-153.us-east-2.compute.internal>
2018-04-26 21:08:28 +0900

    jsontest

--- app/helpers/Parser.rb
+++ app/helpers/Parser.rb
@@ -7,7 +7,7 @@ class Parser
          url ="http://m.gachon.ac.kr/menu/menu.jsp"
             page = RestClient.get(url)
            doc = Nokogiri::HTML(page)
-           info = doc.xpath("//*[@id=\"toggle-view\"]/li[1]/dl/dd[1]")
+           info = doc.xpath("//*[@id=\"toggle-view\"]/li[#{@@day_value}]/dl/dd[1]")
            return info.text.gsub("\r", "\r\n")
     end
     
