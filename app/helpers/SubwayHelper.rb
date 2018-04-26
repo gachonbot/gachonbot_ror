a9c37f00eca3f32014ca8120c62294ce2f717125
EC2 Default User <ec2-user@ip-172-31-36-153.us-east-2.compute.internal>
2018-04-26 23:47:43 +0900

    json

--- app/helpers/SubwayHelper.rb
+++ app/helpers/SubwayHelper.rb
@@ -2,7 +2,6 @@ class SubwayHelper
     
    def show_gachon
         resp = HTTParty.get("http://swopenapi.seoul.go.kr/api/subway/56475774517475673131345a4c714e70/json/realtimeStationArrival/1/5/#{CGI.escape("가천대")}")
-              @updn = Array.new
               @route = Array.new
               @routeMSG = Array.new
                 resp.parsed_response["realtimeArrivalList"].each do |x|
@@ -11,8 +10,6 @@ class SubwayHelper
                 end
                 
         return "#{@route[0]}\n#{@routeMSG[0]}\n\n#{@route[1]}\n#{@routeMSG[1]}\n\n#{@route[2]}\n#{@routeMSG[2]}\n\n#{@route[3]}\n#{@routeMSG[3]}\n"
-        
-       
    end
     
 end
