b1842417ca1ac8b4d7e24e56a96f6edeac1e788c
EC2 Default User <ec2-user@ip-172-31-36-153.us-east-2.compute.internal>
2018-04-29 01:33:59 +0900

    dbtest

--- app/helpers/PhoneHelper.rb
+++ app/helpers/PhoneHelper.rb
@@ -5,11 +5,11 @@ class PhoneHelper
    
    def dept_number(dept)
         dept = dept.split(" ").first
-        @dept_name = @Phones.find_by(name: "#{dept}").name
-        @dept_number = @Phones.find_by(name: "#{dept}").number
         if @Phones.find_by(name: "#{dept}") == nil
            return "없는 과입니다."
         else
+        @dept_name = @Phones.find_by(name: "#{dept}").name
+        @dept_number = @Phones.find_by(name: "#{dept}").number
         return "#{@dept_name}의 과사 전화번호는 \n#{@dept_number} 입니다."
         end
    end
