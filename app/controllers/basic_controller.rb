dd93ed95caf1134dcd0b2ee4655b4b347fa89056
EC2 Default User <ec2-user@ip-172-31-36-153.us-east-2.compute.internal>
2018-04-29 21:45:19 +0900

    test

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -100,7 +100,7 @@ class BasicController < ApplicationController
             render json: jsonHelper.messageJson(phoneHelper.dept_list)
             
         
-        elsif @response.include? "무당이" and @response.include? ("언제와" or "시간")
+        elsif @response.include? "무당이" and @response.include? "언제와"
             render json: jsonHelper.moodangJson
             
             
