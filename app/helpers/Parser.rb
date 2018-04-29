e79c2088af88f6b7ccc114042ae21a7186c55fd8
EC2 Default User <ec2-user@ip-172-31-36-153.us-east-2.compute.internal>
2018-04-29 23:43:35 +0900

    test

--- app/helpers/Parser.rb
+++ app/helpers/Parser.rb
@@ -80,7 +80,7 @@ class Parser
          url ="http://m.gachon.ac.kr/menu/menu.jsp"
             page = RestClient.get(url)
            doc = Nokogiri::HTML(page)
-           info = doc.xpath("//*[@id=\"toggle-view\"]/li[#{(@@day_value+1)}]/dl/dd[1]")
+           info = doc.xpath("//*[@id=\"toggle-view\"]/li[#{day}]/dl/dd[1]")
             return info.text.gsub("\r", "\r\n")
         end
     end
@@ -96,7 +96,7 @@ class Parser
          url ="http://m.gachon.ac.kr/menu/menu.jsp?gubun=B"
             page = RestClient.get(url)
            doc = Nokogiri::HTML(page)
-           info = doc.xpath("//*[@id=\"toggle-view\"]/li[#{(@@day_value+1)}]/dl")
+           info = doc.xpath("//*[@id=\"toggle-view\"]/li[#{day}]/dl")
            return info.text.gsub("\r", "\r\n")
         end
     end
@@ -112,7 +112,7 @@ class Parser
          url ="http://m.gachon.ac.kr/menu/menu.jsp?gubun=C"
             page = RestClient.get(url)
            doc = Nokogiri::HTML(page)
-           info = doc.xpath("//*[@id=\"toggle-view\"]/li[#{(@@day_value+1)}]/dl")
+           info = doc.xpath("//*[@id=\"toggle-view\"]/li[#{day}]/dl")
            return info.text.gsub("\r", "\r\n")
         end
     end
